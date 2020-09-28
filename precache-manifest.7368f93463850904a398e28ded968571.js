158442ca246200a5a6704640f211f7b8534a602e
Barrigan <dcbarrigan@gmail.com>
2020-09-28 15:53:53 +0100

    Updates

---
+++ precache-manifest.7368f93463850904a398e28ded968571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8f8aeb228531a1a434d96e4b61ea51",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "d184a99efd960be1754e",
+    "url": "/drum-machine/static/css/main.9277e0af.chunk.css"
+  },
+  {
+    "revision": "0cf37f800ab8a32c1727",
+    "url": "/drum-machine/static/js/2.544ce722.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/drum-machine/static/js/2.544ce722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d184a99efd960be1754e",
+    "url": "/drum-machine/static/js/main.249f9b96.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
